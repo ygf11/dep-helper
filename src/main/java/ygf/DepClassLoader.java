326873dbda141b6f811942b1418f2842aef05c26
yanggaofeng <yanggf23@gmail.com>
2019-12-10 13:02:41 +0800

    add dep class loader

---
+++ src/main/java/ygf/DepClassLoader.java
@@ -0,0 +1,36 @@
+package ygf;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DepClassLoader extends ClassLoader {
+
+    /**
+     * lib path
+     */
+    private String path;
+
+    /**
+     * classes cache
+     */
+    private Map<String, Class<?>> classCache = new HashMap<String, Class<?>>();
+
+    /**
+     * byte arrays cache
+     */
+    private Map<String, byte[]> bytesCache = new HashMap<String, byte[]>();
+
+    public DepClassLoader(){
+        super();
+    }
+
+    public DepClassLoader(String path){
+        super();
+        this.path = path;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        return super.findClass(name);
+    }
+}
---
+++ src/main/java/ygf/DepContext.java
@@ -0,0 +1,5 @@
+package ygf;
+
+public class DepContext {
+
+}
