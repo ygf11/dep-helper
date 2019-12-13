22e8f1fc69b9c96c59dc1cc9fc75ae5db3b37919
yanggaofeng <yanggf23@gmail.com>
2019-12-13 17:08:39 +0800

    add environment vars

--- dep-helper-maven-plugin/src/main/java/ygf/mvn/plugin/DependenciesMojo.java
+++ dep-helper-maven-plugin/src/main/java/ygf/mvn/plugin/DependenciesMojo.java
@@ -1,12 +1,33 @@
 package ygf.mvn.plugin;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.repository.RemoteRepository;
 
-@Mojo(name = "copy")
+import java.util.List;
+
+@Mojo(name = "move")
 public class DependenciesMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject mavenProject;
+
+    @Parameter(defaultValue = "${session}", readonly = true, required =  true)
+    private MavenSession mavenSession;
+
+    @Component
+    private RepositorySystem repositorySystem;
+
+    @Parameter(defaultValue = "${project.remotePluginRepositories}", readonly = true, required = true)
+    private List<RemoteRepository> remoteRepositories;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
