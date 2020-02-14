d8694c6023e87f0cd4bc52383b384e29ef1625ce
theo <yanggf23@gmail.com>
2020-02-14 16:09:05 +0800

    add example source code

---
+++ example/src/main/java/ygf/deps/example/ExampleMain.java
@@ -0,0 +1,27 @@
+package ygf.deps.example;
+
+import com.dep.service.impl.api.HelloService;
+import ygf.DepContext;
+
+/**
+ * example
+ *
+ * @author yanggaofeng
+ * @date 20200214
+ */
+public class ExampleMain {
+
+    public static void main(String[] args) throws Exception{
+        Class<?>  service1 = DepContext.getClass(
+                "com.dep.service.impl.HelloServiceImpl", "service1-1.0-SNAPSHOT.jar");
+
+        Class<?> service2 = DepContext.getClass(
+                "com.dep.service.impl.HelloServiceImpl", "service2-1.0-SNAPSHOT.jar");
+
+        HelloService first = (HelloService) service1.getConstructor().newInstance();
+        HelloService second = (HelloService) service2.getConstructor().newInstance();
+
+        first.sayHello();
+        second.sayHello();
+    }
+}
